a1b6fefb1bae59e72ba1ad0d78e4861ec2ecbb56
OH <rudals2148@naver.com>
2023-02-02 19:57:44 +0900

    [fix] 페이지 경로 추가

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../pages/Main";
 import Premier from "../pages/Premier";
+import Laliga from "../pages/Laliga";
+import Bundes from "../pages/Bundes";
+import Serie from "../pages/Serie";
+import Ligue from "../pages/Ligue";
 
 const Router = () => {
   return (
@@ -8,6 +12,10 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/premier" element={<Premier />} />
+        <Route path="/laliga" element={<Laliga />} />
+        <Route path="/bundes" element={<Bundes />} />
+        <Route path="/serie" element={<Serie />} />
+        <Route path="/ligue" element={<Ligue />} />
       </Routes>
     </BrowserRouter>
   );
